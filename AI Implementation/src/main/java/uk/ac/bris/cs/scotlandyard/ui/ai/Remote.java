670f0c9fe99750c345d28b9cf7e40815c718b4f4
Henry <henrycourt7@icloud.com>
2025-07-25 16:26:05 +0100

    added files

---
+++ AI Implementation/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/Remote.java
@@ -0,0 +1,9 @@
+package uk.ac.bris.cs.scotlandyard.ui.ai;
+
+
+
+public class Remote {
+    public static void main(String[] args) {
+        uk.ac.bris.cs.scotlandyard.remote.Main.main(args);
+    }
+}
